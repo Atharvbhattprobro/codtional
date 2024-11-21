6517bd021df125c9311ff2f04d857e3e70b96c1f
Atharvbhattprobro <atharvbhattprobro@gmail.com>
2024-11-21 19:19:58 +0400

    first commit

---
+++ a4.py
@@ -0,0 +1,11 @@
+Number1 =float(input("Enter the number1: "))
+Number2 =float(input("Enter the number2: "))
+Number1 =float(input("Enter the number1: "))
+
+if(Number1>Number2):
+    print("Number1 is bigger number")
+
+if(Number1<Number2):
+    print("Number2 is bigger number")
+
+ 
